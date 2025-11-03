9bb9477fdfd28d8c651b49becd92c79ba4382e73
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-11-03 21:43:38 +0300

    Add project files.

---
+++ Academy/Program.cs
@@ -0,0 +1,67 @@
+﻿//#define INHERITANCE_1
+//#define INHERITANCE_2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+	internal class Program
+	{
+		static readonly string delimiter = "\n=====================\n";
+		static void Main(string[] args)
+		{
+#if INHERITANCE_1
+			Console.WriteLine("Academy");
+			Human human = new Human("Montana", "Antonio", 25);
+			human.Info();
+			Console.WriteLine(delimiter);
+
+			Student student = new Student("Pinkman", "Jessie",22,"Chemistry", "WW_220", 90, 95);
+			student.Info();
+			Console.WriteLine(delimiter);
+
+			Teacher teacher = new Teacher("White", "Walter", 50, "Chemistry", 25);
+			teacher.Info();	
+			Console.WriteLine(delimiter);
+#endif
+#if INHERITANCE_2
+			Human human = new Human("Pinkman", "Jessie", 22);
+			human.Info();
+			Console.WriteLine(delimiter);
+
+			Student student = new Student(human, "Chemistry", "WW_220", 90, 95);
+			student.Info();
+			Console.WriteLine(delimiter);
+
+			Teacher teacher = new Teacher(new Human("White", "Walter", 50), "Chemistry", 25);
+			teacher.Info();
+			Console.WriteLine(delimiter);
+
+			Human h_Hank = new Human("Shreder", "Hank", 40);
+			Student s_Hank = new Student(h_Hank, "Criminalistic", "OBN", 50, 60);
+			Graduate graduate = new Graduate(s_Hank, "How to catch Heisenberg");
+			graduate.Info();
+
+#endif
+
+			//base-class pointers: Generalisation (Upcast - приведение дочернего объекта к базовому типу)
+			Human[] group =
+			{
+				new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 90, 95),
+				new Teacher("White", "Walter", 50, "Chemistry", 25),
+				new Graduate("Shreder", "Hank", 40, "Criminalistic", "OBN", 50, 60, "How to catch Heisenberg"),
+				new Student("Vercetty", "Tommy", 30, "Theft", "Vice", 90, 95),
+				new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 25)
+			};
+			for(int i = 0; i < group.Length; i++)
+			{
+				//group[i].Info();
+				Console.WriteLine(group[i].ToString());
+				Console.WriteLine(delimiter);
+			}
+		}
+	}
+}
