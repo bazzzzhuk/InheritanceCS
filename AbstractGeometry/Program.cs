c50b7406409149d25b2bf43eeb43851e1bcea2bb
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-11-09 20:09:42 +0300

    methods add

--- AbstractGeometry/Program.cs
+++ AbstractGeometry/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Runtime.InteropServices; //DLLImport
 using System.Windows.Forms;
 using System.Data;
+using System.Threading;
+
 
 namespace AbstractGeometry
 {
@@ -20,24 +23,75 @@ namespace AbstractGeometry
 				(Console.WindowLeft, Console.WindowTop,
 				Console.WindowWidth, Console.WindowHeight
 				);
-			PaintEventArgs e = new PaintEventArgs( graphics, window_rect );
+			PaintEventArgs e = new PaintEventArgs(graphics, window_rect);
 			//e.Graphics.DrawRectangle(new Pen(Color.Plum,15), 300, 200, 500, 300);
-			Rectangle rectangle = new Rectangle(200, 200, 300, 300, 3, Color.DarkTurquoise);
-			rectangle.Info(e);
+			//Rectangle rectangle = new Rectangle(200, 200, 300, 300, 3, Color.DarkTurquoise);
+			//rectangle.Info(e);
 			// Set world transform of graphics object to rotate.
-			for (float i = 1; i < 360; i++)
-			{
-				e.Graphics.RotateTransform(i);
-
-				// Then to scale, prepending to world transform.
-				e.Graphics.ScaleTransform(3.0F, 2.0F);
-
-				// Draw scaled, rotated rectangle to screen.
-				e.Graphics.DrawRectangle(new Pen(Color.Blue, 3), 150, 150, 100, 40);
-				e.Dispose();
-			}
+			Pen myPen = new Pen(Color.Blue, 10);
+			//System.Drawing.Drawing2D.Matrix rotateMatrix = new System.Drawing.Drawing2D.Matrix();
+			//Flip_cube(e);
+			Flip_ellipse(e);
 		}
 		[DllImport("kernel32.dll")]
 		public static extern IntPtr GetConsoleWindow();
+		public static void Flip_cube(PaintEventArgs e)
+		{
+			Pen myPen = new Pen(Color.Blue, 10);
+			float x = -100.0f;
+			while (true)
+			{
+				for (float i = 10; i < 89; i = i + 1)
+				{
+					Matrix myMatrix = new Matrix();
+					PointF rotatePoint = new PointF(x + 200, 700.0f);
+					myMatrix.RotateAt(i, rotatePoint, MatrixOrder.Append);
+					e.Graphics.DrawRectangle(myPen, x, 500, 200, 200);
+					//Thread.Sleep(1);
+					e.Graphics.Transform = myMatrix;
+					Console.Clear();
+				}
+				x = x + 200;
+			}
+		}
+		public static void Flip_ellipse(PaintEventArgs e)
+		{
+			Pen pen = new Pen(Color.DarkOliveGreen, 1);
+			Brush brush = new SolidBrush(Color.DarkGreen);
+
+			// Create points that define polygon.
+			PointF point1 = new PointF(325.0F, 2.5F);
+			PointF point2 = new PointF(375.0F, 2.5F);
+			PointF point3 = new PointF(375.0F, 125.0F);
+			PointF point4 = new PointF(400.0F, 125.0F);
+			PointF point5 = new PointF(350.0F, 175.0F);
+			PointF point6 = new PointF(302.5F, 125.0F);
+			PointF point7 = new PointF(325.0F, 125.0F);
+			PointF[] curvePoints =
+					 {
+				 point1,
+				 point2,
+				 point3,
+				 point4,
+				 point5,
+				 point6,
+				 point7
+			 };
+			Random random = new Random();
+			while (true)
+			{
+				for (float i = 0; i < 361; i = i + 2)
+				{
+					Matrix myMatrix = new Matrix();
+					PointF rotatePoint = new PointF(350, 300.0f);
+					myMatrix.RotateAt(i, rotatePoint, MatrixOrder.Append);
+					//Thread.Sleep(1);
+					e.Graphics.Transform = myMatrix;
+					Console.Clear();
+					e.Graphics.DrawPolygon(pen, curvePoints);
+					e.Graphics.FillEllipse(brush, 250, 200, 200, 200);
+				}
+			}
+		}
 	}
 }
